6c96d1a3a67bc2e9b656c860500aa885eb8d9f53
Patrick Flood <109551677+JustCuzImMario@users.noreply.github.com>
2023-04-14 23:22:53 -0600

    updated

---
+++ static/index.js
@@ -0,0 +1,27 @@
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const searchResults = document.querySelector("#search-results");
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const searchQuery = searchInput.value;
+    fetch(`https://api.rawg.io/api/games?search=${searchQuery}`)
+        .then((response) => response.json())
+        .then((data) => {
+            searchResults.innerHTML = "";
+            data.results.forEach((game) => {
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("game-card");
+
+        const gameTitle = document.createElement("h2");
+        gameTitle.innerText = game.name;
+
+        const gameCover = document.createElement("img");
+        gameCover.src = game.background_image;
+
+        gameCard.appendChild(gameTitle);
+        gameCard.appendChild(gameCover);
+        searchResults.appendChild(gameCard);
+      });
+    });
+});
