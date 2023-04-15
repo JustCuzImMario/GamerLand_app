7698a43c62d6e7989be42c97fd96d0e30968a372
Patrick Flood <109551677+JustCuzImMario@users.noreply.github.com>
2023-04-14 23:08:03 -0600

    updated

---
+++ static/index2.js
@@ -0,0 +1,52 @@
+const { search } = require("../routes/games");
+
+fetch('/games')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+fetch('http://localhost:3000/games/popular')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const searchResults = document.querySelector('#search-results');
+
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const query = searchInput.value;
+
+    try {
+    const response = await fetch(`/search?query=${query}`);
+    const games = await response.json();
+
+    // Clear the previous search results
+    searchResults.innerHTML = '';
+
+    // Display the search results
+    games.forEach((game) => {
+        const gameContainer = document.createElement('div');
+        gameContainer.className = 'game-container';
+
+        const title = document.createElement('h3');
+        title.textContent = game.title;
+        gameContainer.appendChild(title);
+
+        const description = document.createElement('p');
+        description.textContent = game.description;
+        gameContainer.appendChild(description);
+
+        const image = document.createElement('img');
+        image.src = game.imageUrl;
+        gameContainer.appendChild(image);
+
+        searchResults.appendChild(gameContainer);
+    });
+    } catch (error) {
+    console.error(error);
+    }
+});
+
+export { search };
+    
