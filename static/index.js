9056c89b8ee03741315b561c8b7636f70037fa01
Patrick Flood <109551677+JustCuzImMario@users.noreply.github.com>
2023-04-15 00:43:39 -0600

    update

--- static/index.js
+++ static/index.js
@@ -1,28 +1,47 @@
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const apiKey = "c0c4266a6e0c49218d68459d4798adc7";
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 const searchResults = document.querySelector("#search-results");
 
 searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const searchQuery = searchInput.value;
-    fetch(`https://api.rawg.io/api/games?search=${searchQuery}`)
-        .then((response) => response.json())
-        .then((data) => {
-            searchResults.innerHTML = "";
-            data.results.forEach((game) => {
-                const gameCard = document.createElement("div");
-                gameCard.classList.add("game-card");
+  event.preventDefault();
+  const gameName = searchInput.value;
+  const apiUrl = `${proxyUrl}https://api.rawg.io/api/games?search=${gameName}&key=${apiKey}`;
 
-                const gameTitle = document.createElement("h2");
-                gameTitle.innerText = game.name;
-
-                const gameCover = document.createElement("img");
-                gameCover.src = game.background_image;
-
-                gameCard.appendChild(gameTitle);
-                gameCard.appendChild(gameCover);
-                searchResults.appendChild(gameCard);
-            });
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.results.length > 0) {
+        searchResults.innerHTML = "";
+        const gameCardContainer = document.createElement("div");
+        gameCardContainer.classList.add("game-card-container");
+        data.results.forEach((game) => {
+          const gameCard = document.createElement("div");
+          gameCard.classList.add("game-card");
+      
+          const gameTitle = document.createElement("h2");
+          gameTitle.innerText = game.name;
+      
+          const gameCover = document.createElement("img");
+          gameCover.src = game.background_image;
+      
+          gameCard.appendChild(gameTitle);
+          gameCard.appendChild(gameCover);
+          gameCardContainer.appendChild(gameCard);
         });
+        searchResults.appendChild(gameCardContainer);
+      } else {
+        searchResults.innerHTML = "No games found!";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      searchResults.innerHTML = "Error fetching games!";
+    });
 });
-
