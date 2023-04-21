976da93f64b0aa4d197b3be7b9a811ff53cb8398
Patrick Flood <109551677+JustCuzImMario@users.noreply.github.com>
2023-04-21 08:10:00 -0600

    updated

--- static/index.js
+++ static/index.js
@@ -3,6 +3,8 @@ const apiKey = "c0c4266a6e0c49218d68459d4798adc7";
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 const searchResults = document.querySelector("#search-results");
+const MongoClient = require('mongodb').MongoClient;
+
 
 
 // Search for games
@@ -98,6 +100,42 @@ function getGameDetails(gameId, descriptionLength) {
       .catch(error => console.log(error));
   }
   
+
+
+
+  
+// Connecting to MongoDB for Login and Account Creation
+
+const uri = "mongodb+srv://patchflood17:Tydetmer14!@clustergl.niradnb.mongodb.net/?retryWrites=true&w=majority"; 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log('Connected to database!');
+  // perform database operations here
+});
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
   
