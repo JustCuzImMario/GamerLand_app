c6d9f286e51a1d10238a864dead4571f694e384e
Patrick Flood <109551677+JustCuzImMario@users.noreply.github.com>
2023-04-25 11:22:08 -0600

    updated

--- static/server.js
+++ static/server.js
@@ -23,32 +23,43 @@ app.listen(port, () => {
 const fetch = require('node-fetch');
 const VideoGame = require('./models/VideoGame');
 
-app.get('/games', async (req, res) => {
-  const gameName = req.query.name;
-  const apiUrl = `https://api.rawg.io/api/games?search=${gameName}&key=${apiKey}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    const games = data.results.map((game) => {
-      return {
-        _id: game.id.toString(), // add _id field and convert to string
-        name: game.name,
-        coverImage: game.background_image, // change background_image to coverImage
-        description: game.description_raw, // change description_raw to description
-        releaseDate: game.released, // add releaseDate field
-        platforms: game.platforms.map((platform) => platform.platform.name), // add platforms field
-        genres: game.genres.map((genre) => genre.name), // add genres field
-        developers: game.developers.map((developer) => developer.name), // add developers field
-      };
-    });
-
-    await VideoGame.insertMany(games);
-
-    res.json({ message: 'Video games added successfully' }); // update success message
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error fetching video games' }); // update error message
-  }
-});
+app.get('/games/:id', async (req, res) => {
+    const gameId = req.params.id;
+    
+    try {
+      // Check if game exists in MongoDB
+      const game = await Game.findOne({ _id: gameId });
+      
+      if (game) {
+        // If game exists in MongoDB, send it as response
+        res.json(game);
+      } else {
+        // If game doesn't exist in MongoDB, fetch it from RAWG.io API
+        const apiUrl = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        
+        // Map RAWG.io game data to match MongoDB schema
+        const newGame = {
+          _id: data.id.toString(),
+          name: data.name,
+          coverImage: data.background_image,
+          description: data.description_raw,
+          releaseDate: data.released,
+          platforms: data.platforms.map((platform) => platform.platform.name),
+          genres: data.genres.map((genre) => genre.name),
+          developers: data.developers.map((developer) => developer.name),
+        };
+        
+        // Save new game to MongoDB
+        const savedGame = await Game.create(newGame);
+        
+        // Send new game as response
+        res.json(savedGame);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching video game' });
+    }
+  });
+  
